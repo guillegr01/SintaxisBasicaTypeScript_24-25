451170b9af8e4ec9d1577c70ea7f58db6600a2ff
guillegr01 <gllrmglrncn2001@gmail.com>
2026-01-21 19:33:20 +0100

    actualizando el fichero Objetos.ts (unnion e intersection types)

--- objetos.ts
+++ objetos.ts
@@ -24,3 +24,39 @@ const claves = Object.keys(yo);//devuelve los atributos que tiene el objeto(name
 claves.forEach((elem) => {
     console.log(yo[elem as keyof Persona]); //Para imprimir los valores de los atributos del objeto
 })
+
+//Union types
+type HeroPowerScale = 'local' | 'planetary' | 'galactic';
+type HeroId = `${string}-${string}-${string}-${string}-${string}`;
+
+//Intersection types
+
+type HeroBasicInfo = {
+    name: string
+    age:number
+}
+
+type HeroProperties = {
+    readonly id: HeroId
+    isActive?: boolean
+    powerScale: HeroPowerScale
+}
+
+type Hero = HeroBasicInfo & HeroProperties;
+
+const CreateHero = (basicInfo: HeroBasicInfo, props: HeroProperties): Hero => {
+
+    const {name, age} = basicInfo;
+    const {isActive, powerScale} = props;
+
+    return {
+        name: name,
+        age: age,
+        id:crypto.randomUUID(),
+        isActive: isActive,
+        powerScale: powerScale
+    };
+
+}
+
+console.log(JSON.stringify(CreateHero({name: 'Thor',age: 27},{id: crypto.randomUUID(), isActive: true, powerScale: "planetary" })));
